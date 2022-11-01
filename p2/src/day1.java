2ff69efe3ccbb1dc680db53de613606a90335920
ragasudha72 <111664908+ragasudha72@users.noreply.github.com>
2022-11-01 21:24:44 +0530

    Update day1.java

--- p2/src/day1.java
+++ p2/src/day1.java
@@ -1,9 +1,6 @@
-import java.util.*;
 public class day1 {
 	public static void main(String args[]) {
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter your name:");
-		String word=s.nextLine();
-		System.out.println("Hello "+word);
+		System.out.println("Name:Raga Sudha");
+		System.out.println("Sector 2D road,Puppalguda,Manikonda");
 	}
 }

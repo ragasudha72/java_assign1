baddc4e68258478f7c16ae993fdadf931cdceeaf
ragasudha72 <111664908+ragasudha72@users.noreply.github.com>
2022-11-01 21:22:23 +0530

    Update prog2.java

--- p2/src/prog2.java
+++ p2/src/prog2.java
@@ -1,7 +1,10 @@
-
+import java.util.*;
 public class prog2 {
 	public static void main(String args[] ){
-		System.out.println("HI");
+		String name;
+		Scanner s=new Scanner(System.in);
+		name=s.nextLine();
+		System.out.println("Hello"+name);
 	}
 
 }
